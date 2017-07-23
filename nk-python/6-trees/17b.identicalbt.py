47d5ac83e4286ea61c1548d561b745f2c766bcf9
namanagr <namanagr@gmail.com>
2017-07-23 13:21:00 -0700

    latest commit from thinkpad

--- nk-python/6-trees/17b.identicalbt.py
+++ nk-python/6-trees/17b.identicalbt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This program finds if two trees are structurally identical or not
+# This program finds if two trees are identical or not
 
 class TreeNode:
     def __init__(self, val):
@@ -7,11 +7,11 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def struc_indetical(root1, root2):
+def btindetical(root1, root2):
     if root1 is None and root2 is None:
         return True
     elif root1 is not None and root2 is not None:
-        return struc_indetical(root1.left,root2.left) & struc_indetical(root1.right,root2.right)
+        return (root1.val == root2.val) & btindetical(root1.left,root2.left) & btindetical(root1.right,root2.right)
     else:
         return False
 
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     root2.left = TreeNode(2)
     root2.right = TreeNode(3)
     root2.left.left = TreeNode(4)
-    root2.right.right = TreeNode(15)
-    print struc_indetical(root,root2)
+    root2.right.right = TreeNode(10)
+    print btindetical(root,root2)
